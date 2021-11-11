caa3e62f00292814a8ec98046cd31bb8a7d03403
Kelly Davis <kldavis4@gmail.com>
2021-11-11 14:26:19 -0500

    fix mobile display of header

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ import fencerSalute from '../public/fencer_salute.png'
 
 export function Logo() {
   return (
-    <div className="mx-auto px-3">
+    <div className="mx-auto px-3 hidden sm:block md:block lg:block">
       <img src={fencerSalute.src} alt="Fencer saluting" height={60} width={15} />
     </div>
   )
